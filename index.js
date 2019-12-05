109666801f0443a99f930e6a17d16de31a457631
Andrew Hartness <ahartness@amtdirect.com>
2019-12-05 02:36:12 -0500

    Added Bencode
    data serialization format

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 const fs = require('fs');
-const torrent = fs.readFileSync('puppy.torrent');
-console.log(torrent.toString('utf8'));
+const bencode = require('bencode');
+
+const torrentString = fs.readFileSync('puppy.torrent');
+console.log(torrentString.toString('utf8'));
+
+const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
+const trackerUrl = torrent.announce.toString('utf8');
+
+
+
