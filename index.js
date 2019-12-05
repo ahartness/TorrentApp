cac6589899b75b37fd0e8ce0fa0d7f53a57e63ae
Andrew Hartness <ahartness@amtdirect.com>
2019-12-05 02:45:25 -0500

    Bencode plus gitignore

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const fs = require('fs');
 const bencode = require('bencode');
 
 const torrentString = fs.readFileSync('puppy.torrent');
-console.log(torrentString.toString('utf8'));
+console.log(torrentString.toString('utf8'));            // Full Torrent File String
 
 const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
 const trackerUrl = torrent.announce.toString('utf8');
+console.log(trackerUrl);                                // This is our UDP
+
+
 
 
 
